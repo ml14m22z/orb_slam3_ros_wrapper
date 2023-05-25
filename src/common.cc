e24600ab3519befec101713798b3d119d28349ee
Moke Zhang <17319370515@163.com>
2023-05-25 10:12:06 +0800

    remove redundant header in common.cc

--- src/common.cc
+++ src/common.cc
@@ -5,13 +5,6 @@
 */
 
 #include "common.h"
-#include "System.h"
-#include "publisher.h"
-#include "image_transport.h"
-#include "PoseStamped.h"
-#include "PointCloud2.h"
-#include "tf/LinearMath/Transform.h"
-#include "tf/transform_broadcaster.h"
 
 ORB_SLAM3::System::eSensor sensor_type;
 std::string world_frame_id, cam_frame_id, imu_frame_id;
